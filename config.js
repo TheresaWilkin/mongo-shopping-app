16e3a939c7ddaa365b3ca0a4870d35b1a6da75dc
TheresaWilkin <theresawilkin@yahoo.com>
2016-11-21 16:28:11 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 exports.DATABASE_URL = 'mongodb://shoppinguser:shop@ds159507.mlab.com:59507/mongo-shopping-app';
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = 'https://theresawilkin.github.io/mongo-shopping-app/' || 8080;
